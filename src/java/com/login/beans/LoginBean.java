db52ff286af46045fb8fb3ea9eb03195ea0d371a
marin <marin@marin-pc>
2020-05-01 16:08:50 -0500

    renamed vista del login y añadiendo acciones

--- src/java/com/login/beans/LoginBean.java
+++ src/java/com/login/beans/LoginBean.java
@@ -15,11 +15,37 @@ import java.io.Serializable;
  */
 @Named(value = "loginBean")
 @SessionScoped
+
 public class LoginBean implements Serializable {
     // Atributo para ocultar/visualizar panel login
     
     private boolean verPanelLogin;
+    //Atributo para capturar el texto para mostrar el saludo
+    private String  textoSaludo="";
+    //Atributo contador para los saludos
+    private short contadorSaludos=0;
 
+    public short getContadorSaludos() {
+        return contadorSaludos;
+    }
+
+    public void setContadorSaludos(short contadorSaludos) {
+        this.contadorSaludos = contadorSaludos;
+    }
+
+    public LoginBean(boolean verPanelLogin) {
+        this.textoSaludo = "";
+        this.verPanelLogin = verPanelLogin;
+    }
+
+    public String getTextoSaludo() {
+        return textoSaludo;
+    }
+
+    public void setTextoSaludo(String textoSaludo) {
+        this.textoSaludo = textoSaludo;
+    }
+    
     public boolean isVerPanelLogin() {
         return verPanelLogin;
     }
@@ -37,12 +63,20 @@ public class LoginBean implements Serializable {
      * Creates a new instance of loginBean
      */
     public LoginBean() {
+        this.textoSaludo = "";
         this.verPanelLogin = false;
     }
+    
+    public void aumentarContadorSaludos(){
+        contadorSaludos++;
+    }
+            
+    
     
     //Metodo para cambiar el valor de la variable y ocultar o mostrar el panel
     public void habilitarOdeshabilitarLogin(){
         
         verPanelLogin =! verPanelLogin;
+        
     }
 }
